108f7aa9a426f2c788ddc5ac34be5939ca29e195
TextGuySemicolon <junyanbb1@gmail.com>
2022-07-09 12:22:22 +0800

    the elders

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import './fonts/CartoonOneFont.css';
 import BrownConnect from './images/BrownConnect.png';
 import Brown from './images/Brown.png';
 import Banner from './images/BannerT.png';
+import TheFather from './images/The Father.png';
+import TheLittleMan from './images/The Little Man.png';
+import TheHomunculus from './images/The Homunculus.png';
 const config: Config = {
   readOnlyChainId: ChainId.Mainnet,
   readOnlyUrls: {
@@ -78,6 +81,33 @@ function App() {
 
         <div style={{ padding: '1vw' }}></div>
 
+      </div>
+
+      <div className="SectionThree">
+        <div style={{ padding: '1vw' }}></div>
+        <div id='FAQ' className="TitleHeader" style={{ fontSize: '6vw' }}>𝔗ℌ𝔈 𝔈𝔏𝔇𝔈ℜ𝔖</div>
+        <div className='FlexBoxes' style={{ textAlign: 'left', verticalAlign: 'middle', color: 'white', margin: 'auto', width: "90vw", height: "42vw" }}>
+
+          <div className="BlackSection" style={{ width: "24.4vw", borderRadius: '1vw', margin: '1vw', padding: '.5vw', boxShadow: "0px 0px 1vw rgba(24,  22 ,33,.5)" }}>
+            <div className="TitleHeader" style={{ padding: '1vw', fontSize: '2vw' }}>“ 𝔗𝔥𝔢 𝔏𝔦𝔱𝔱𝔩𝔢 𝔐𝔞𝔫”</div>
+            <img src={TheLittleMan} style={{ borderWidth: '0.2vw', borderColor: 'black', borderStyle: 'solid', width: '20vw', margin: '0vw 2vw', borderRadius: '1vw' }}></img>
+            <div style={{ padding: '.5vw', fontSize: '1.5vw', marginTop: '1vw' }}>“ 𝔗𝔥𝔢 𝔏𝔦𝔱𝔱𝔩𝔢 𝔐𝔞𝔫” 𝔦𝔰 𝔱𝔥𝔢 𝔣𝔦𝔯𝔰𝔱 𝔰𝔲𝔠𝔠𝔢𝔰𝔰𝔣𝔲𝔩 𝔥𝔬𝔪𝔲𝔫𝔠𝔲𝔩𝔲𝔰 𝔱𝔥𝔞𝔱 𝔦𝔰 𝔠𝔯𝔢𝔞𝔱𝔢𝔡 𝔟𝔶 𝔣𝔞𝔱𝔥𝔢𝔯 𝔴𝔦𝔱𝔥 𝔱𝔥𝔢 𝔯𝔢𝔡 𝔰𝔱𝔬𝔫𝔢, 𝔩𝔞𝔱𝔢𝔯 𝔨𝔫𝔬𝔴𝔫 𝔞𝔰 “𝔓𝔥𝔦𝔩𝔬𝔰𝔬𝔭𝔥𝔢𝔯’𝔰 𝔖𝔱𝔬𝔫𝔢”, 𝔦𝔱 𝔠𝔞𝔫 𝔱𝔞𝔩𝔨 𝔞𝔫𝔡 𝔦𝔰 𝔢𝔵𝔱𝔯𝔢𝔪𝔢𝔩𝔶 𝔦𝔫𝔱𝔢𝔩𝔩𝔦𝔤𝔢𝔫𝔱, 𝔞𝔩𝔰𝔬 𝔭𝔬𝔰𝔰𝔢𝔰𝔰𝔢𝔡 𝔰𝔬𝔪𝔢 𝔨𝔦𝔫𝔡 𝔬𝔣 𝔪𝔶𝔰𝔱𝔢𝔯𝔦𝔬𝔲𝔰 𝔞𝔟𝔦𝔩𝔦𝔱𝔦𝔢𝔰.</div>
+          </div>
+
+          <div className="BlackSection" style={{ width: "24.4vw", borderRadius: '1vw', margin: '1vw', padding: '.5vw', boxShadow: "0px 0px 1vw rgba(24,  22 ,33,.5)" }}>
+            <div className="TitleHeader" style={{ padding: '1vw', fontSize: '2vw' }}>“ 𝔗𝔥𝔢 𝔉𝔞𝔱𝔥𝔢𝔯”</div>
+            <img src={TheFather} style={{ borderWidth: '0.2vw', borderColor: 'black', borderStyle: 'solid', width: '20vw', margin: '0vw 2vw', borderRadius: '1vw' }}></img>
+            <div style={{ padding: '.5vw', fontSize: '1.5vw', marginTop: '1vw' }}>" 𝔗𝔥𝔢 𝔉𝔞𝔱𝔥𝔢𝔯" 𝔦𝔰 𝔠𝔯𝔲𝔢𝔩 𝔱𝔬 𝔥𝔲𝔪𝔞𝔫, 𝔟𝔲𝔱 𝔶𝔢𝔱 𝔨𝔦𝔫𝔡 𝔱𝔬 𝔞𝔫𝔦𝔪𝔞𝔩𝔰 𝔞𝔫𝔡 𝔦𝔫𝔰𝔢𝔠𝔱𝔰, 𝔥𝔢 𝔠𝔞𝔫 𝔲𝔰𝔢 𝔥𝔲𝔪𝔞𝔫 𝔬𝔯 𝔥𝔲𝔪𝔞𝔫 𝔟𝔩𝔬𝔬𝔡 𝔱𝔬 𝔣𝔲𝔰𝔢 𝔞𝔫𝔡 𝔱𝔯𝔶𝔦𝔫𝔤 𝔱𝔬 𝔠𝔯𝔢𝔞𝔱𝔢 𝔭𝔥𝔦𝔩𝔬𝔰𝔬𝔭𝔥𝔢𝔯𝔰 𝔰𝔱𝔬𝔫𝔢 𝔟𝔲𝔱 𝔴𝔬𝔫’𝔱 𝔭𝔦𝔠𝔨 𝔞 𝔣𝔩𝔬𝔴𝔢𝔯 𝔣𝔯𝔬𝔪 𝔱𝔥𝔢 𝔤𝔞𝔯𝔡𝔢𝔫, 𝔟𝔲𝔱 𝔩𝔬𝔳𝔦𝔫𝔤 𝔥𝔦𝔰 𝔥𝔬𝔪𝔦𝔢𝔰 𝔠𝔯𝔢𝔞𝔱𝔦𝔬𝔫𝔰.</div>
+          </div>
+
+          <div className="BlackSection" style={{ width: "24.4vw", borderRadius: '1vw', margin: '1vw', padding: '.5vw', boxShadow: "0px 0px 1vw rgba(24,  22 ,33,.5)" }}>
+            <div className="TitleHeader" style={{ padding: '1vw', fontSize: '2vw' }}>“ 𝔗𝔥𝔢 ℌ𝔬𝔪𝔲𝔫𝔠𝔲𝔩𝔲𝔰”</div>
+            <img src={TheHomunculus} style={{ borderWidth: '0.2vw', borderColor: 'black', borderStyle: 'solid', width: '20vw', margin: '0vw 2vw', borderRadius: '1vw' }}></img>
+            <div style={{ padding: '.5vw', fontSize: '1.5vw', marginTop: '1vw' }}>𝔒𝔫𝔢 𝔬𝔣 𝔱𝔥𝔢 1 𝔬𝔣 1 " 𝔗𝔥𝔢 ℌ𝔬𝔪𝔲𝔫𝔠𝔲𝔩𝔲𝔰" 𝔦𝔰 𝔬𝔫𝔢 𝔬𝔣 𝔱𝔥𝔢 𝔪𝔬𝔰𝔱 𝔰𝔲𝔠𝔠𝔢𝔰𝔰𝔣𝔲𝔩 𝔠𝔯𝔢𝔞𝔱𝔦𝔬𝔫 𝔬𝔣 𝔥𝔦𝔰, 𝔪𝔞𝔨𝔦𝔫𝔤 𝔱𝔥𝔢 𝔞𝔭𝔭𝔢𝔞𝔯𝔞𝔫𝔠𝔢 𝔞𝔩𝔪𝔬𝔰𝔱 𝔞𝔰 𝔠𝔩𝔬𝔰𝔢 𝔱𝔬 𝔞 𝔫𝔬𝔯𝔪𝔞𝔩 𝔥𝔲𝔪𝔞𝔫.</div>
+          </div>
+
+        </div>
+        <div style={{ padding: '3vw' }}></div>
       </div>
 
       <div className='mainHeader' style={{ height: '5vw' }}>
